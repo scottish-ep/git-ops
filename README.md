e215898e3559d32ad321a6ee4dc2538392a57966
scottish-ep <scottish.foldep@gmail.com>
2022-09-07 20:25:49 +0700

    git ops

---
+++ README.md
@@ -0,0 +1,17 @@
+# Hello world docker action
+
+This action allow you to restart your k8s deployment, now for testing purposes, 2 main feature
+- omit the `image-name` and it will do the restart for your deployment
+- or set the `image-name` for it to run set the image to you deployment
+
+## Inputs
+
+## `server-url`
+## `token`
+## `deployment`
+## `image-name`
+## `namespace`
+
+## Outputs
+
+no for now
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -l
+
+set -e
+
+GIT_TOKEN=$4
+GIT_REPO=$5
+
+TAG=$1
+DEPLOYMENT_FILE=$2
+
+IMAGE_REPO=$3
+MATCH_PATTERN=$6
+
+GIT_REPO_URL="https://scottish-ep:$GIT_TOKEN@$GIT_REPO"
+
+git clone $GIT_REPO_URL
+
+ls -la
+sed "s/$MATCH_PATTERN/$TAG/g" $DEPLOYMENT_FILE
+sed -i "s/$MATCH_PATTERN/$TAG/g" $DEPLOYMENT_FILE
+
+git config --global user.name "pepepot"
+git config --global user.email "ops@eastplayers.io"
+git config --global color.ui true
+
+git add $DEPLOYMENT_FILE
+git commit -m "[ci] Update $IMAGE_REPO to $TAG"
+git push origin main
